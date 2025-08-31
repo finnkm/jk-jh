f0eb88e2a2e4dfd49f8e678727c7457365c44f0a
finn.kim <thefinnkim@gmail.com>
2025-08-31 21:46:42 +0900

    apply themes

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
+import path from "path";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -41,6 +42,11 @@ export default defineConfig(({ mode }) => {
       }),
     ],
     base: "/jk-jh/",
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
     define: {
       __SITE_TITLE__: JSON.stringify(env.SITE_TITLE || ""),
       __SITE_DESCRIPTION__: JSON.stringify(env.SITE_DESCRIPTION || ""),
