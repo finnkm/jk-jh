c69ed32188751838cbcedd23e19fa95c4c6af61e
finn.kim <thefinnkim@gmail.com>
2025-09-04 21:14:32 +0900

    update FooterSection

--- src/components/sections/FooterSection.tsx
+++ src/components/sections/FooterSection.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export const FooterSection: React.FC = () => {
   return (
     <footer className="w-full flex items-center justify-between gap-4 mt-auto">
-      <p>{import.meta.env.VITE_DATE}</p>
-      <p>이 모바일 청첩장은 개발자 신랑이 직접 제작했습니다.</p>
+      <p className="text-xs">{import.meta.env.VITE_DATE}</p>
+      <p className="text-xs">이 모바일 청첩장은 개발자 신랑이 직접 제작했습니다.</p>
     </footer>
   );
 };
